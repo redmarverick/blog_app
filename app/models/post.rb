3d62d3ccbf99620ae2c057d81e56281bc51fa825
redmarverick <nyldrift@hotmail.com>
2023-08-23 17:02:35 -0300

    Setup controllers
    - Create controllers
    - Setup routes
    - Create Views

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_create :increment_posts_counter
+  after_destroy :decrement_posts_counter
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
@@ -16,6 +18,16 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
+  end
+  def update_comments_counter
+    update_column(:comments_counter, comments.count)
+  end
   def update_likes_counter
     update_column(:likes_counter, likes.count)
   end
