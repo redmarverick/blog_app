40cd15f4daa230ff824844a7ec63b0979eafcd31
redmarverick <nyldrift@hotmail.com>
2023-08-26 05:07:01 -0300

    New funcionalities added
    - create new posts funcionality
    - add new comments funcionality
    - add new likes functionality
    - add current user functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,12 +10,14 @@ class Post < ApplicationRecord
 
   after_create :increment_posts_counter
   after_destroy :decrement_posts_counter
+
+  scope :ordered_by_id_desc, -> { order(id: :desc) }
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
 
   def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :asc).limit(5)
   end
 
   def increment_posts_counter
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
   root to: "users#index"
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show], controller: 'user_posts'
+    resources :posts, only: [:index, :show, :new, :create], controller: 'user_posts' do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create], controller: 'user_post_likes'
+    end
+  end
+
+  resources :posts do
+    member do
+      post 'like'
+    end
+    resources :comments, only: [:create]
+  end
+
+  resources :user_posts, only: [:index, :show, :new, :create] do
+    resources :likes, only: [:create], controller: 'user_post_likes'
   end
 end
