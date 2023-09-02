fe212cba488485c51e6c4a31a7ce26afa0f1a1e1
redmarverick <nyldrift@hotmail.com>
2023-09-02 02:44:29 -0300

    API Endpoint
    - create API endpoint for comments and posts

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,26 @@
+class Api::CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+
+    render json: @comments, status: :ok
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:author, :comments).find(params[:post_id]) # Eager load author and comments
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
